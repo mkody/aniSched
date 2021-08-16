49ba6e183e422b682622643a58e169fecb94c44b
MKody <im@kdy.ch>
2021-08-16 13:52:52 +0000

    Oh hey it looks better with ceil()

--- index.php
+++ index.php
@@ -95,8 +95,8 @@ foreach ($period as $dt) {
         }
         $airingSun = count($shows->airing) - $airingSat;
 
-        // I'm just trying things to make the number of shows close to even on Sat. and Sun.
-        $min = floor((count($shows->catchup) - ($airingSun - $airingSat) - $airingSat) / 2);
+        // I'm trying to make the number of shows close to even on Sat. and Sun.
+        $min = ceil((count($shows->catchup) - ($airingSun - $airingSat) - $airingSat) / 2);
     }
 ?>
         <h3 id="<?= $dt->format("d") ?>"><?= $dt->format("l d") ?></h3>
